493ef44edaa7f63587acc372d8acbde3baaae479
zikrisuanda11 <zikrisuanda998@gmail.com>
2024-10-15 17:59:11 +0800

    update: adding analytics vercel config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["cyrillic-ext"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextTopLoader showSpinner={false} />
         {children}
+        <Analytics />
       </body>
     </html>
   );
